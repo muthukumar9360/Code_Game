0633933bef88482728a95528b1a530e4ea7c4870
FrankieeGodwin <frankgdn431@gmail.com>
2025-12-08 15:44:41 +0530

    Tailwind CSS Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="text-red-900 font-bold text-4xl" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
