3963c2231651655ff900d771496de427fe561aab
muthukumar9360 <perumpatayans@gmail.com>
2025-12-09 11:57:10 +0530

    Routes Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import Home from "./Pages/Home.jsx";
 import Login from "./Pages/Login.jsx";
 import Signup from "./Pages/Signup.jsx";
 import RoomLobby from "./Pages/RoomLobby.jsx";
+import ContestPage from "./Pages/ContestPage.jsx";
+import ResultPage from "./Pages/ResultPage.jsx";
+import CreateRoom from "./Pages/CreateRoom.jsx";
+import JoinRoom from "./Pages/JoinRoom.jsx";
 
 
 function App() {
@@ -12,8 +16,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<Home />} />
-
+        <Route path="/create-room" element={<CreateRoom />} />
+        <Route path="/join-room" element={<JoinRoom />} />
+        <Route path="/room/:roomId" element={<RoomLobby />} />
+        <Route path="/contest/:contestId" element={<ContestPage />} />
+        <Route path="/results/:contestId" element={<ResultPage />} />
       </Routes>
     </BrowserRouter>
   );
