2d033a5444f2a094b904191a7c33cfe925dda6d2
muthukumar9360 <perumpatayans@gmail.com>
2025-12-09 11:52:52 +0530

    Values

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import Login from "./Pages/Login.jsx";
 import Signup from "./Pages/Signup.jsx";
+import RoomLobby from "./Pages/RoomLobby.jsx";
+
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Home />} />
+
       </Routes>
     </BrowserRouter>
   );
